56be3561d503cb48d27a4db7f80894564dbf6a02
Thomas Peterson <tap6@email.sc.edu>
2024-11-14 09:03:39 -0500

    Implemented needed pom.xml dependencies & imported requirements in module-info

--- lovelearninglangs/src/main/java/module-info.java
+++ lovelearninglangs/src/main/java/module-info.java
@@ -1,6 +1,17 @@
 module thecoolcoders {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
+    requires junit;
+    requires jlayer;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.services.polly;
+    requires software.amazon.awssdk.regions;
+    requires software.amazon.awssdk.utils;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires software.amazon.awssdk.awscore;
+    requires software.amazon.eventstream;
 
     opens thecoolcoders to javafx.fxml;
     exports thecoolcoders;
