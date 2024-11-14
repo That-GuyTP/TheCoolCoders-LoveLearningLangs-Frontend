1156f2c3723673db59a6e7c8189dcd0072a4a281
Thomas Peterson <tap6@email.sc.edu>
2024-11-14 09:33:59 -0500

    App.java now uses the Narrator

--- lovelearninglangs/src/main/java/com/controllers/PrimaryController.java
+++ lovelearninglangs/src/main/java/com/controllers/PrimaryController.java
@@ -1,15 +1,16 @@
 package com.controllers;
 
 import java.io.IOException;
-
 import com.application.App;
-
 import javafx.fxml.FXML;
+import com.narration.*;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
+        System.out.println("You clicked the primary button!");
+        Narrator.playSound("Hallo Thomas! Wie gehts?");
         App.setRoot("secondary");
     }
 }
