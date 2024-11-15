998e04a8e81e9916da40e4fb6eb52071ded431bf
Thomas Peterson <tap6@email.sc.edu>
2024-11-15 10:25:17 -0500

    Fixed mistype in psuedocode

--- lovelearninglangs/src/main/java/com/controllers/LoginSubmit.java
+++ lovelearninglangs/src/main/java/com/controllers/LoginSubmit.java
@@ -7,14 +7,14 @@ import javafx.fxml.FXML;
 public class LoginSubmit {
 
     /* When you click the LoginSubmit button it will:
-     1. Print to the console, "This is the submit button! Taking you back to the startup page!"
+     1. Print to the console, "This is the submit button! Taking you back to homepage page!"
      2. Switch back to the homepage page.
 
      Alex, I believe this is where you will input the logic for checking a user's information is correct and confirming them logging in so that the app switches to the homepage.
      */
     @FXML
     private void switchToHomePage() throws IOException {
-        System.out.println("This is the submit button! Taking you back to the startup page!");
+        System.out.println("This is the submit button! Taking you to the homepage page!");
         App.setRoot("homepage");
     }
 }
