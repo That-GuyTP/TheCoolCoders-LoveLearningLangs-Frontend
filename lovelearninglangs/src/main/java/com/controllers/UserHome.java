33780f062155568e2f5eb596ccd17179cebc8799
jaervin1 <jaerin@email.sc.edu>
2024-12-02 21:47:36 -0500

    Userhome logic additions

--- lovelearninglangs/src/main/java/com/controllers/UserHome.java
+++ lovelearninglangs/src/main/java/com/controllers/UserHome.java
@@ -32,42 +32,55 @@ import javafx.stage.Stage;
 import com.model.*;
 import java.util.HashMap;
 
-public class UserHome implements Initializable{
+public class UserHome implements Initializable {
 
     private LikeLearningLangs lll;
     private User currentUser;
-    
-    @FXML private Label welcomeLabel;
-    @FXML private VBox userLanguageList;
+
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private VBox userLanguageList;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         lll = LikeLearningLangs.getInstance();
+        if (!lll.isLoggedIn) {
+            try {
+                switchToStartup();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         currentUser = lll.getCurrentUser();
 
         welcomeLabel.setText("Lets get Started, " + currentUser.getFirstName());
         displayUserItems();
     }
 
-    private void displayUserItems(){
+    private void displayUserItems() {
         HashMap<Language, Double> userProgress = currentUser.getProgress();
-        ArrayList<Language> userLanguages = new ArrayList<>();
-        for(Language language: userProgress.keySet()){
-            userLanguages.add(language);
+
+        userLanguageList.getChildren().clear();
+        for (Language language : userProgress.keySet()) {
             HBox hBox = new HBox();
             Image image = new Image(getClass().getResourceAsStream("/images/" + language.label + ".jpg"));
             ImageView languageImage = new ImageView(image);
             languageImage.setFitHeight(50);
+            languageImage.setFitWidth(70);
             languageImage.setPreserveRatio(true);
             hBox.getChildren().add(languageImage);
 
             userLanguageList.getChildren().add(hBox);
 
             Button languageButton = new Button(language.toString());
-            hBox.getChildren().add(languageImage);
-            languageButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            languageButton.setFont(new Font(18));
+            languageButton.setPrefHeight(50);
+            hBox.getChildren().add(languageButton);
+
+            languageButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
-                public void handle(MouseEvent event){
+                public void handle(MouseEvent event) {
                     lll.getCourse(language);
                     try {
                         switchToCourse();
@@ -75,23 +88,51 @@ public class UserHome implements Initializable{
                         e.printStackTrace();
                     }
                 }
-                
+
             });
         }
+
+        Button addLanguageButton = new Button("Add Language");
+        addLanguageButton.setFont(new Font(18));
+        addLanguageButton.setPrefHeight(50);
+
+        userLanguageList.getChildren().add(addLanguageButton);
+        addLanguageButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    switchToAddLanguage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        HBox addLanguageHBox = new HBox();
+        addLanguageHBox.setSpacing(10);
+        addLanguageHBox.getChildren().add(addLanguageButton);
+
+        userLanguageList.getChildren().add(addLanguageHBox);
     }
 
     @FXML
-    private void switchToCourse() throws IOException{
+    private void switchToCourse() throws IOException {
         App.setRoot("course");
     }
 
     @FXML
-    private void switchToAddLanguage() throws IOException{
+    private void switchToAddLanguage() throws IOException {
         App.setRoot("addlanguage");
     }
-    
+
     @FXML
-    private void switchToProfile() throws IOException{
+    private void switchToProfile() throws IOException {
         App.setRoot("profile");
+    }
+
+    @FXML
+    private void switchToStartup() throws IOException {
+        App.setRoot("startup");
     }
 }
