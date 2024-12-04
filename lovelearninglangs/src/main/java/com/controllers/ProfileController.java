4746f241d799f367a2e3ebdf9fd45297e5677406
jaervin1 <jaervin@email.sc.edu>
2024-12-04 14:36:26 -0500

    Userhome and Profile controller additions

---
+++ lovelearninglangs/src/main/java/com/controllers/ProfileController.java
@@ -0,0 +1,51 @@
+package com.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.application.App;
+import com.model.LikeLearningLangs;
+import com.model.User;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ProfileController implements Initializable{
+
+    @FXML
+    private Label fullNameLabel, usernameLabel;
+
+    @FXML
+    private TextField userFirstName, userLastName, userUsername, userEmail;
+
+    @FXML
+    private PasswordField userPassword;
+
+    private User currentUser;
+    private LikeLearningLangs lll;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        lll = LikeLearningLangs.getInstance();
+        if (!lll.isLoggedIn) {
+            try {
+                switchToStartup();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        currentUser = lll.getCurrentUser();
+    }
+
+    @FXML
+    private void switchToStartup() throws IOException {
+        App.setRoot("startup");
+    }
+
+    
+
+}
